cc60f4d03ba8166e5281522f2d6c87a606ac6e4c
Alex Godino <alexgodino.ag@gmail.com>
2023-11-20 13:10:30 +0100

    Menu principal, casi menu proyectos y login

--- src/CodigoFuente/Inmobiliaria.java
+++ src/CodigoFuente/Inmobiliaria.java
@@ -1,5 +1,7 @@
 package CodigoFuente;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Inmobiliaria {
@@ -23,6 +25,7 @@ public class Inmobiliaria {
             default -> "";
         };
     }
+
     public static String contrasena (int opcion) {
         return switch (opcion) {
             case 1 -> "administrador";
@@ -33,7 +36,6 @@ public class Inmobiliaria {
         };
     }
 
-
     public static boolean acceso (String usu, String con) {
         System.out.println("Introduzca su usario:");
         String user = leerOpcionLiteral();
@@ -43,15 +45,40 @@ public class Inmobiliaria {
     }
 
     //mostrarProyectos
+    public static void menuProyectos(){
+        System.out.println("Seleccione que desea realizar con los proyectos: \n1. Mostrar proyectos creados.\n2. Crear nuevo proyecto.\n3. Salir");
+    }
 
-
+    public static String creaProyectos(){
+        System.out.print("Introduzca el nombre del proyecto: ");
+        String nombreProyecto= leerOpcionLiteral();
+        System.out.print("Introduzca una descripción para el proyecto: ");
+        String descripcionProyecto= leerOpcionLiteral();
+        System.out.print("Introduzca el tipo del proyecto (si es préstamo, plusvalía o alquiler): ");
+        String tipoProyecto= leerOpcionLiteral();
+        System.out.print("Introduzca la cantidad necesaria para este proyecto: ");
+        String cantidadNecesariaProyecto= leerOpcionLiteral();
+        System.out.print("Introduzca la cantidad financiada hasta el momento: ");
+        String cantidadFinanciadaProyecto= leerOpcionLiteral();
+        System.out.print("Introduzca la fecha inicial del proyecto: ");
+        String fechaInicialProyecto= leerOpcionLiteral();
+        System.out.print("Introduzca la fecha final del proyecto: ");
+        String fechaFinalProyecto= leerOpcionLiteral();
+        
+    }
 
     public static void main(String[] args) {
         menuUser();
         int opcion1 = leerOpcionNumerica();
+        int opcion2;
         String usuario = usuario(opcion1), contrasenia = contrasena(opcion1);
-        if (acceso(usuario, contrasenia)) System.out.println("YES");
+        if (acceso(usuario, contrasenia)) System.out.println("YES"); //Cambiar y poner que sea hasta 3 veces como máximo
         else System.out.println("NO");
-
+        if(opcion1==2) {
+            menuProyectos();
+            if(leerOpcionNumerica()==2){
+                creaProyectos();
+            }
+        }
     }
 }
