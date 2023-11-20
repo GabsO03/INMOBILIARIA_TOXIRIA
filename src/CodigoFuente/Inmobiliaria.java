97b6b72ae525f369d873f4b4a1239aa089c70638
Alex Godino <alexgodino.ag@gmail.com>
2023-11-20 17:11:02 +0100

    Menu principal, casi menu proyectos y login

--- src/CodigoFuente/Inmobiliaria.java
+++ src/CodigoFuente/Inmobiliaria.java
@@ -47,36 +47,37 @@ public class Inmobiliaria {
         System.out.println("Seleccione que desea realizar con los proyectos: \n1. Mostrar proyectos creados.\n2. Crear nuevo proyecto.\n3. Salir");
     }
 
-    public static String creaProyectos(){
-        System.out.print("Introduzca el nombre del proyecto: ");
-        String nombreProyecto= leerOpcionLiteral();
-        System.out.print("Introduzca una descripción para el proyecto: ");
-        String descripcionProyecto= leerOpcionLiteral();
-        System.out.print("Introduzca el tipo del proyecto (si es préstamo, plusvalía o alquiler): ");
-        String tipoProyecto= leerOpcionLiteral();
-        System.out.print("Introduzca la cantidad necesaria para este proyecto: ");
-        String cantidadNecesariaProyecto= leerOpcionLiteral();
-        System.out.print("Introduzca la cantidad financiada hasta el momento: ");
-        String cantidadFinanciadaProyecto= leerOpcionLiteral();
-        System.out.print("Introduzca la fecha inicial del proyecto: ");
-        String fechaInicialProyecto= leerOpcionLiteral();
-        System.out.print("Introduzca la fecha final del proyecto: ");
-        String fechaFinalProyecto= leerOpcionLiteral();
-        
-    }
-
     public static void main(String[] args) {
-        menuUser();
-        int opcion1 = leerOpcionNumerica();
-        int opcion2;
-        String usuario = usuario(opcion1), contrasenia = contrasena(opcion1);
-        if (acceso(usuario, contrasenia)) System.out.println("YES"); //Cambiar y poner que sea hasta 3 veces como máximo
-        else System.out.println("NO");
-        if(opcion1==2) {
-            menuProyectos();
-            if(leerOpcionNumerica()==2){
-                creaProyectos();
+        do {
+            menuUser();
+            int opcion1 = leerOpcionNumerica();
+            int opcion2, contadorProyectos = 1;
+            String usuario = usuario(opcion1), contrasenia = contrasena(opcion1);
+            if (acceso(usuario, contrasenia))
+                System.out.println("YES"); //Cambiar y poner que sea hasta 3 veces como máximo
+            else System.out.println("NO");
+            if (opcion1 == 2) {
+                menuProyectos();
+                if (leerOpcionNumerica() == 2) {
+                    do {
+                        contadorProyectos++;
+                        System.out.print("Introduzca el nombre del proyecto: ");
+                        String nombreProyecto1 = leerOpcionLiteral();
+                        System.out.print("Introduzca una descripción para el proyecto: ");
+                        String descripcionProyecto1 = leerOpcionLiteral();
+                        System.out.print("Introduzca el tipo del proyecto (si es préstamo, plusvalía o alquiler): ");
+                        String tipoProyecto1 = leerOpcionLiteral();
+                        System.out.print("Introduzca la cantidad necesaria para este proyecto: ");
+                        String cantidadNecesariaProyecto1 = leerOpcionLiteral();
+                        System.out.print("Introduzca la cantidad financiada hasta el momento: ");
+                        String cantidadFinanciadaProyecto1 = leerOpcionLiteral();
+                        System.out.print("Introduzca la fecha inicial del proyecto: ");
+                        String fechaInicialProyecto1 = leerOpcionLiteral();
+                        System.out.print("Introduzca la fecha final del proyecto: ");
+                        String fechaFinalProyecto1 = leerOpcionLiteral();
+                    }while(contadorProyectos<=3);
+                }
             }
-        }
+        }while(leerOpcionNumerica()!=4);
     }
 }
