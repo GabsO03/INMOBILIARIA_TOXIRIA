71876dff4dcff50b436c345d90bedae025ecb760
Alex Godino <alexgodino.ag@gmail.com>
2023-12-12 14:01:40 +0100

    Más javadoc realizado

--- src/Biblioteca/Inversiones.java
+++ src/Biblioteca/Inversiones.java
@@ -1,6 +1,15 @@
 package Biblioteca;
 
 public class Inversiones {
+
+    /**
+     * Nos muestra algunas características de las inversiones de los inversores en cada uno de los
+     * proyectos en los que ha invertido
+     * @author Adrián Contreras Bueno
+     * @param nombreProyecto como cadena
+     * @param tipoProyecto como cadena
+     * @param cantidadParticipada como cadena
+     */
     public static void mostrarInversionEnProyecto(String nombreProyecto, String tipoProyecto, double cantidadParticipada){
         System.out.println("Nombre: "+nombreProyecto);
         System.out.println("Tipo: "+tipoProyecto);
--- src/Biblioteca/Lectura_De_Datos.java
+++ src/Biblioteca/Lectura_De_Datos.java
@@ -3,14 +3,32 @@ package Biblioteca;
 import java.util.Scanner;
 
 public class Lectura_De_Datos {
+
+    /**
+     * Leemos un número entero introducido por pantalla
+     * @author Gabriela Oria Pinto y Alex Godino Bailen
+     * @return un número entero introducido por pantalla
+     */
     public static int leerOpcionNumerica () {
         Scanner escaner = new Scanner(System.in);
         return Integer.parseInt(escaner.nextLine());
     }
+
+    /**
+     * Leemos un número decimal introducido por pantalla
+     * @author Gabriela Oria Pinto y Alex Godino Bailen
+     * @return un número decimal introducido por pantalla
+     */
     public static double leerOpcionDouble () {
         Scanner escaner = new Scanner(System.in);
         return Double.parseDouble(escaner.nextLine());
     }
+
+    /**
+     * Leemos una cadena introducida por pantalla
+     * @author Gabriela Oria Pinto y Alex Godino Bailen
+     * @return una cadena introducida por pantalla
+     */
     public static String leerOpcionLiteral () {
         Scanner escaner = new Scanner(System.in);
         return escaner.nextLine();
