a2ee066a9ffcaff8818fb4aa6c4680d3d6e2c1e3
Alex Godino <alexgodino.ag@gmail.com>
2023-12-17 15:19:09 +0100

    javadoc terminado

--- src/Biblioteca/Menus.java
+++ src/Biblioteca/Menus.java
@@ -1,27 +1,63 @@
 package Biblioteca;
 
 public class Menus {
+    /**
+     * Función la cual nos muestra el menu inicial, el menú para iniciar sesión
+     * @author Alex Godino Bailen
+     */
     public static void menuUser () {
         System.out.println("Seleccione su tipo de usuario\n1. Administrador.\n2. Gestor.\n3. Inversor1.\n4. Inversor2\n5. Salir");
     }
+
+    /**
+     * Función para mostrar el menú de los proyectos realizados por el gestor.
+     * @author Alex Godino Bailen
+     */
     public static void menuProyectosGestor(){
         System.out.println("Seleccione que desea realizar con los proyectos: \n1. Mostrar proyectos creados.\n2. Crear nuevo proyecto.\n3. Configuración.\n4. Salir");
     }
+
+    /**
+     * Función para mostrar el menú del administrador
+     * @author Alex Godino Bailen
+     */
     public static void menuAdministrador(){
         System.out.println("Seleccione dónde quiere acceder: \n1.Panel de control\n2.Proyectos\n3.Configuración\n4.Salir");
     }
+
+    /**
+     * Función para mostrar el menú de para que el administrador pueda modificar los proyectos creados por el gestor
+     * @author Gabriela Oria Pinto
+     */
     public static void menuModificarProyecto(){
         System.out.println("Elija qué quiere modificar:\n1.Nombre\n2.Descripcion\n3.Tipo\n4.Cantidad necesaria\n5.Cantidad Financiada" +
                 "\n6.Fecha inicio\n7.Fecha fin\n8.Nada");
     }
+
+    /**
+     * Función para mostrar el menu del inversor
+     * @author Gabriela Oria Pinto
+     */
     public static void menuInversor(){
         System.out.println("Seleccione donde quiere acceder: \n1. Panel de Control\n2. Mis inversiones\n3. Proyectos\n4. Cartera Virtual\n5. Configuración\n6. Salir");
     }
+
+    /**
+     * Función para mostrar el menú de cambio de usuario y cambio de contraseña
+     * @author Adrian Contreras Bueno
+     */
     public static void menuConfiguracion(){
         System.out.println("Seleccione qué quiere hacer:");
         System.out.println("1. Cambiar usuario\n2. Cambiar contraseña\n3.Salir");
     }
 
+    /**
+     * Función para mostrar el menú de las inversiones realizadas por los inversores
+     * @author Adrian Contreras Bueno
+     * @param nombreProyecto1 como cadena
+     * @param nombreProyecto2 como cadena
+     * @param nombreProyecto3 como cadena
+     */
     public static void menuInversiones(String nombreProyecto1,String nombreProyecto2,String nombreProyecto3){
         System.out.println("Seleccione en qué proyecto quiere invertir: ");
         System.out.printf("1.%s\n2.%s\n3.%s\n4.Salir\n",nombreProyecto1,nombreProyecto2,nombreProyecto3);
--- src/Biblioteca/Proyectos.java
+++ src/Biblioteca/Proyectos.java
@@ -4,6 +4,22 @@ import static Biblioteca.Lectura_De_Datos.leerOpcionDouble;
 import static Biblioteca.Lectura_De_Datos.leerOpcionLiteral;
 
 public class Proyectos {
+    /**
+     * Función para poder mostrar los proyectos a aquellos usuario que no sean los administradores
+     * @author Adrian Contreras Bueno y Alex Godino Bailen
+     * @param nombreProyecto1 como cadena
+     * @param nombreProyecto2 como cadena
+     * @param nombreProyecto3 como cadena
+     * @param tipoProyecto1 como cadena
+     * @param tipoProyecto2 como cadena
+     * @param tipoProyecto3 como cadena
+     * @param cantidadNecesariaProyecto1 como numero real
+     * @param cantidadNecesariaProyecto2 como numero real
+     * @param cantidadNecesariaProyecto3 como numero real
+     * @param cantidadFinanciadaProyecto1 como numero real
+     * @param cantidadFinanciadaProyecto2 como numero real
+     * @param cantidadFinanciadaProyecto3 como numero real
+     */
     public static void mostrarProyectosNoAdmin(String nombreProyecto1, String nombreProyecto2, String nombreProyecto3, String tipoProyecto1, String tipoProyecto2, String tipoProyecto3,
                                                double cantidadNecesariaProyecto1, double cantidadNecesariaProyecto2, double cantidadNecesariaProyecto3,
                                                double cantidadFinanciadaProyecto1, double cantidadFinanciadaProyecto2, double cantidadFinanciadaProyecto3){
@@ -13,6 +29,23 @@ public class Proyectos {
         System.out.printf("Cantidad Necesaria: %.2f\t %.2f\t %.2f\n",cantidadNecesariaProyecto1,cantidadNecesariaProyecto2,cantidadNecesariaProyecto3);
         System.out.printf("Cantidad Financiada: %.2f\t %.2f\t %.2f\n",cantidadFinanciadaProyecto1,cantidadFinanciadaProyecto2,cantidadFinanciadaProyecto3);
     }
+
+    /**
+     * Función para poder mostrar los proyectos al administrador
+     * @author Adrian Contreras Bueno y Alex Godino Bailen
+     * @param nombreProyecto1 como cadena
+     * @param nombreProyecto2 como cadena
+     * @param nombreProyecto3 como cadena
+     * @param tipoProyecto1 como cadena
+     * @param tipoProyecto2 como cadena
+     * @param tipoProyecto3 como cadena
+     * @param cantidadNecesariaProyecto1 como numero real
+     * @param cantidadNecesariaProyecto2 como numero real
+     * @param cantidadNecesariaProyecto3 como numero real
+     * @param cantidadFinanciadaProyecto1 como numero real
+     * @param cantidadFinanciadaProyecto2 como numero real
+     * @param cantidadFinanciadaProyecto3 como numero real
+     */
     public static void mostrarProyectosAdmin(String nombreProyecto1, String nombreProyecto2, String nombreProyecto3, String tipoProyecto1, String tipoProyecto2, String tipoProyecto3,
                                              double cantidadNecesariaProyecto1, double cantidadNecesariaProyecto2, double cantidadNecesariaProyecto3,
                                              double cantidadFinanciadaProyecto1, double cantidadFinanciadaProyecto2, double cantidadFinanciadaProyecto3){
@@ -25,57 +58,130 @@ public class Proyectos {
         System.out.println("Elija cual quiere eliminar o modificar: \n1. Proyecto 1\n2. Proyecto 2\n3. Proyecto 3\n4.Ver más detalles\n5. Ninguno");
     }
 
+    /**
+     * Función para cambiar el nombre de algún proyecto
+     * @author Alex Godino Bailen y Adrián Contreras Bueno
+     * @return una cadena con el nuevo nombre del proyecto introducido por consola
+     */
+
     public static String cambiarNombreProyecto(){
         System.out.println("Escriba el nuevo nombre del proyecto: ");
         return leerOpcionLiteral();
     }
+    /**
+     * Función para cambiar la descripcion de algún proyecto
+     * @author Alex Godino Bailen y Adrián Contreras Bueno
+     * @return una cadena con la nueva descripción del proyecto introducida por consola
+     */
     public static String cambiarDescripcionProyecto(){
         System.out.println("Escriba la nueva descripción del proyecto: ");
         return leerOpcionLiteral();
     }
+    /**
+     * Función para cambiar el tipo de algún proyecto
+     * @author Alex Godino Bailen y Adrián Contreras Bueno
+     * @return una cadena con el nuevo tipo del proyecto introducido por consola
+     */
     public static String cambiarTipoProyecto(){
         System.out.println("Escriba el nuevo tipo del proyecto");
         return leerOpcionLiteral();
     }
+    /**
+     * Función para cambiar la cantidad necesaria de algún proyecto
+     * @author Alex Godino Bailen y Adrián Contreras Bueno
+     * @return un número real con la nueva cantidad necesaria del proyecto introducida por consola
+     */
     public static double cambiarCantidadNecesaria (){
         System.out.println("Escriba la nueva cantidad necesaria");
         return leerOpcionDouble();
     }
+    /**
+     * Función para cambiar la cantidad financiada hasta el momento de algún proyecto
+     * @author Alex Godino Bailen y Adrián Contreras Bueno
+     * @return un número real con la nueva cantidad financiada hasta el momento del proyecto introducida por consola
+     */
     public static double cambiarCantidadFinanciada (){
         System.out.println("Escriba la nueva cantidad financiada");
         return leerOpcionDouble();
     }
+    /**
+     * Función para cambiar la fecha inicial de algún proyecto
+     * @author Alex Godino Bailen y Adrián Contreras Bueno
+     * @return una cadena con la nueva fecha inicial del proyecto introducida por consola
+     */
     public static String cambiarFechaInicio(){
         System.out.println("Escriba la nueva fecha de inicio");
         return leerOpcionLiteral();
     }
+    /**
+     * Función para cambiar la fecha final de algún proyecto
+     * @author Alex Godino Bailen y Adrián Contreras Bueno
+     * @return una cadena con la nueva fecha final del proyecto introducida por consola
+     */
     public static String cambiarFechaFin(){
         System.out.println("Escriba la nueva fecha de fin");
         return leerOpcionLiteral();
     }
 
-    //
+    /**
+     * Función para crear un proyecto en donde leemos el nombre, que es introducido por consola
+     * @author Adrián Contreras Bueno y Alex Godino Bailen
+     */
     public static void crearNombreProyecto(){
         System.out.print("Introduzca el nombre del proyecto: ");
     }
+
+    /**
+     * Función para crear un proyecto en donde leemos la descripción, que es introducido por consola
+     * @author Adrián Contreras Bueno y Alex Godino Bailen
+     */
     public static void crearDescripcionProyecto(){
         System.out.print("Introduzca la descripción del proyecto: ");
     }
+
+    /**
+     * Función para crear un proyecto en donde leemos el tipo, que es introducido por consola
+     * @author Adrián Contreras Bueno y Alex Godino Bailen
+     */
     public static void crearTipoProyecto(){
         System.out.print("Introduzca el Tipo del proyecto (Préstamo, Plusvalía o Alquiler): ");
     }
+
+    /**
+     * Función para crear un proyecto en donde leemos la cantidad necesaria, que es introducido por consola
+     * @author Adrián Contreras Bueno y Alex Godino Bailen
+     */
     public static void crearCNecesariaProyecto(){
         System.out.print("Introduzca la cantidad necesaria del proyecto: ");
     }
+    /**
+     * Función para crear un proyecto en donde leemos la cantidad financiada hasta el momento, que es introducido por consola
+     * @author Adrián Contreras Bueno y Alex Godino Bailen
+     */
     public static void crearCFinanciadaProyecto(){
         System.out.print("Introduzca la cantidad financiada hasta el momento del proyecto: ");
     }
+    /**
+     * Función para crear un proyecto en donde leemos su fecha inicial, que es introducido por consola
+     * @author Adrián Contreras Bueno y Alex Godino Bailen
+     */
     public static void crearFInicioProyecto(){
         System.out.print("Introduzca la fecha de inicio del proyecto (dd/mm/aaaa): ");
     }
+    /**
+     * Función para crear un proyecto en donde leemos su fecha final, que es introducido por consola
+     * @author Adrián Contreras Bueno y Alex Godino Bailen
+     */
     public static void crearFFinProyecto(){
         System.out.print("Introduzca la fecha final del proyecto (dd/mm/aaaa): ");
     }
+
+    /**
+     * Función para calcular y mostrar el gráfico de las inversiones que se realizan para cada proyecto
+     * @author Adrián Contreras Bueno
+     * @param cantidadNecesaria como número real
+     * @param cantidadFinanciada como número real
+     */
     public static void crearGrafico(double cantidadNecesaria,double cantidadFinanciada){
         int porcentaje= (int) ((int) (cantidadFinanciada*100)/cantidadNecesaria);
         String caracterLleno="#";
@@ -85,6 +191,32 @@ public class Proyectos {
         System.out.print(caracterVacio.repeat((100-porcentaje)));
 
     }
+
+    /**
+     * Función para mostrar todos los detalles que tiene un proyecto
+     * @author Adrián Contreras Bueno y Alex Godino Bailen
+     * @param nombreProyecto1 como cadena
+     * @param nombreProyecto2 como cadena
+     * @param nombreProyecto3 como cadena
+     * @param descripcionProyecto1 como cadena
+     * @param descripcionProyecto2 como cadena
+     * @param descripcionProyecto3 como cadena
+     * @param tipoProyecto1 como cadena
+     * @param tipoProyecto2 como cadena
+     * @param tipoProyecto3 como cadena
+     * @param cantidadNecesariaProyecto1 como número real
+     * @param cantidadNecesariaProyecto2 como número real
+     * @param cantidadNecesariaProyecto3 como número real
+     * @param cantidadFinanciadaProyecto1 como número real
+     * @param cantidadFinanciadaProyecto2 como número real
+     * @param cantidadFinanciadaProyecto3 como número real
+     * @param fechaInicioProyecto1 como cadena
+     * @param fechaInicioProyecto2 como cadena
+     * @param fechaInicioProyecto3 como cadena
+     * @param fechaFinProyecto1 como cadena
+     * @param fechaFinProyecto2 como cadena
+     * @param fechaFinProyecto3 como cadena
+     */
     public static void proyectosDetallados(String nombreProyecto1,String nombreProyecto2,String nombreProyecto3,String descripcionProyecto1,String descripcionProyecto2,
                                            String descripcionProyecto3,String tipoProyecto1,String tipoProyecto2, String tipoProyecto3,
                                            double cantidadNecesariaProyecto1,double cantidadNecesariaProyecto2,double cantidadNecesariaProyecto3,
