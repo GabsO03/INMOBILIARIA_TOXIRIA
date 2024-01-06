bd55af0796ee7c60d24f45372db663d78aaf823c
GabsO03 <gabrielaoria66@gmail.com>
2024-01-06 20:37:23 +0100

    Refactorización de usuarios, falta fusionar arrays.

--- src/Biblioteca/Login.java
+++ src/Biblioteca/Login.java
@@ -51,156 +51,94 @@ public class Login {
     /**
      * Nos indica si el usuario y la contraseña que estamos introduciendo son correctos o no
      * @author Gabriela Oria Pinto
-     * @param usu como cadena
-     * @param con como cadena
      * @return true si el usuario y la contraseña son correctos o false si el usuario y la contraseña no son correctos
      */
-    public static boolean verification (String usu, String con) {
-        System.out.println("Introduzca su usuario:");
-        String user = leerOpcionLiteral();
-        System.out.println("Introduzca su contraseña");
-        String password = leerOpcionLiteral();
-        return user.equals(usu) && password.equals(con);
-    }
-
-    /**
-     * Sistema de login del administrador
-     * @author Gabriela Oria Pinto
-     * @param user como cadena
-     * @param con como cadena
-     * @return true si ha conseguido entrar al sistema
-     */
-    public static boolean loginAdmin (String user, String con) {
-        while (!(verification(user, con))) System.out.println("Usuario o contraseña incorrecta.");
-        System.out.println("Bienvenid@ " + user);
-        return true;
-    }
-
-    /**
-     * Sistema de login del Gestor
-     * @author Gabriela Oria Pinto
-     * @param user como cadena
-     * @param pass como cadena
-     * @return true si ha conseguido entrar al sistema o false si no ha conseguido entrar al sistema porque su usuario ha sido bloqueado
-     */
-    public static boolean loginGestor (String[]todosUsuarios ,String user, String pass,String[] arraycorreos) {
-        int indiceParaCorreo=0;
-        for (int i = 0; i < todosUsuarios.length; i++) {
-            if (todosUsuarios[i].equals(user)){
-                indiceParaCorreo=i;
-                break;
-            }
-        }
-        int intentos = 3;
-        while (intentos > 0 && !(verification(user, pass))) {
-            intentos--;
-            System.out.println("Contraseña incorrecta, Te quedan " + intentos + " intentos.");
-        }
-        if (intentos > 0) System.out.println();
-        else {
-            System.out.println("Acceso denegado, tu cuenta ha sido bloqueada, espera a que el administrador te desbloquee.");
-            return false;
-        }
+    public static boolean tokenVerified(String[][] datosNoAdmin, int pos) {
         System.out.println("Se va a enviar un código de verificacion a tu correo electronico, un momento...");
         int codigoRandom = (int) (Math.random()*9999)+1000;
         String codigoString=String.valueOf(codigoRandom);
-        String destinatario=arraycorreos[indiceParaCorreo];
+        String destinatario=datosNoAdmin[3][pos];
         String asunto="Código de verificación inmobiliaria";
         String cuerpo="Su código de verificación es: "+codigoString+"\nNo comparta este código con nadie";
         enviarCorreo(destinatario,asunto,cuerpo);
 
         System.out.println("Escriba su código de verificación que se le ha enviado al correo electrónico: ");
         String codigo=leerOpcionLiteral();
-        if (codigo.equals(codigoString)) {
-            System.out.println("Código correcto.");
-            return true;
-        }
-        else{
-            System.out.println("Error, código no valido.");
-            return false;
-        }
-
+        if (codigo.equals(codigoString)) return true;
+        else return false;
 
     }
 
     /**
-     * Sistema de login de ambos inversores
+     * Sistema de login del administrador
      * @author Gabriela Oria Pinto
-     * @param user como cadena
-     * @param pass como cadena
-     * @return true si ha conseguido entrar al sistema o false si no ha conseguido entrar al sistema porque su usuario ha sido bloqueado
+     * @return true si ha conseguido entrar al sistema
      */
-
-    public static boolean loginInversor(String[]todosUsuarios ,String user, String pass, String[] arraycorreos) {
-        int indiceParaCorreo=0;
-        for (int i = 0; i < todosUsuarios.length; i++) {
-            if (todosUsuarios[i].equals(user)){
-                indiceParaCorreo=i;
-                break;
+    public static boolean loginAdmin (String[][] datosAdmins) {
+        boolean correcto = false, userFound = false;
+        int i = 0;
+        do {
+            System.out.println("Usuario");
+            String usuarioAdmin = leerOpcionLiteral();
+            System.out.println("Contra");
+            String contraAdmin = leerOpcionLiteral();
+            while (!userFound && i < datosAdmins[0].length) {
+                if (usuarioAdmin.equals(datosAdmins[1][i])) userFound=true;
+                else i++;
             }
-        }
-
-        int intentos = 3;
-        while (intentos > 0 && !(verification(user, pass))) {
-            intentos--;
-            System.out.println("Contraseña incorrecta, Te quedan " + intentos + " intentos.");
-        }
-        if (intentos > 0) System.out.println();
-        else {
-            System.out.println("Acceso denegado, tu cuenta ha sido bloqueada, espera a que el administrador te desbloquee.");
-            return false;
-        }
-        System.out.println("Se va a enviar un código de verificacion a tu correo electronico, un momento...");
-        int codigoRandom = (int) (Math.random()*9999)+1000;
-        if (user.equals("inversor1")){
-            String codigoString=String.valueOf(codigoRandom);
-            String destinatario=arraycorreos[indiceParaCorreo];
-            String asunto="Código de verificación inmobiliaria";
-            String cuerpo="Su código de verificación es: "+codigoString+"\nNo comparta este código con nadie";
-            enviarCorreo(destinatario,asunto,cuerpo);
-
-            System.out.println("Escriba su código de verificación que se le ha enviado al correo electrónico: ");
-            String codigo=leerOpcionLiteral();
-            if (codigo.equals(codigoString)) {
-                System.out.println("Código correcto.");
-                return true;
+            if (userFound&&(contraAdmin.equals(datosAdmins[2][i]))) {
+                System.out.printf("Bienvenid@ %s.\n", datosAdmins[0][i]);
+                correcto=true;
             }
-            else{
-                System.out.println("Error, código no valido.");
-                return false;
-            }
-        }
-        return false;
+            else System.out.println("Usuario o contraseña incorrectos, vuelva a intentarlo.");
+        } while (!correcto);
+        return true;
     }
 
+    /**
+     * Sistema de login del Gestor
+     * @author Gabriela Oria Pinto
+     * @return true si ha conseguido entrar al sistema o false si no ha conseguido entrar al sistema porque su usuario ha sido bloqueado
+     */
+    public static boolean loginNoAdmin (String[][] datosNoAdmins) {
+        boolean correcto = false, userFound = false;
+        int i = 0, intentos = 3;
+        do {
+            intentos--;
+            System.out.println("Usuario");
+            String usuarioAdmin = leerOpcionLiteral();
+            System.out.println("Contra");
+            String contraAdmin = leerOpcionLiteral();
+            while (!userFound && i < datosNoAdmins[0].length) {
+                if (usuarioAdmin.equals(datosNoAdmins[1][i])) userFound=true;
+                else i++;
+            }
+            if (userFound&&(contraAdmin.equals(datosNoAdmins[2][i]))) {
+                while (!tokenVerified(datosNoAdmins, i)) System.out.println("Error, código incorrecto");
+                System.out.printf("Bienvenid@ %s.\n", datosNoAdmins[0][i]);
+                correcto=true;
+            }
+            else System.out.println("Usuario o contraseña incorrectos, le quedan " + intentos + " intentos.");
+            if (intentos==0) {
+                System.out.println("Acceso denegado, tu cuenta ha sido bloqueada, espera a que el administrador te desbloquee.");
+                return false;
+            }
+
+        } while (!correcto && intentos > 0);
+        return true;
+    }
     /**
      * Función que utiliza las otras funciones de login para acceder al sistema. Esta se usa en el main
      * @author Gabriela Oria Pinto, Adrián Contreras Bueno y Alex Godino Bailen
-     * @param opcion1 como entero
-     * @param adminUsers como array de cadena
-     * @param gestorUsers como array de cadena
-     * @param inversorUsers como array de cadena
-     * @param adminPass como array de cadena
-     * @param gestorpass como array de cadena
-     * @param inversorpass como array de cadena
      * @return true si ha conseguido entrar al sistema
      */
 
-    public static boolean logged (int opcion1,String[] adminUsers,String[] gestorUsers,String[] inversorUsers,String[] adminPass,String[] gestorpass,String[] inversorpass,String[] arraycorreosGestor,String[] arraycorreosInversor) {
-        switch (opcion1) {
-            case 1,2 : {
-                if (loginAdmin(adminUsers[opcion1-1], adminPass[opcion1-1])) return true;
-                break;
-            }
-            case 3,4,5,6 : {
-                if (loginGestor(gestorUsers,gestorUsers[opcion1-3], gestorpass[opcion1-3], arraycorreosGestor)) return true;
-                break;
-            }
-            case 7,8,9,10 : {
-                if (loginInversor(inversorUsers,inversorUsers[opcion1-5], inversorpass[opcion1-5], arraycorreosInversor)) return true;
-                break;
-            }
-        }
-        return false;
+    public static boolean login(int tipo, String[][] datosAdmin, String[][] datosGestores, String[][] datosInversores) {
+        return switch (tipo) {
+            case 1 -> loginAdmin(datosAdmin);
+            case 2 -> loginNoAdmin(datosGestores);
+            case 3 -> loginNoAdmin(datosInversores);
+            default -> throw new IllegalStateException("Unexpected value: " + tipo);
+        };
     }
 }
